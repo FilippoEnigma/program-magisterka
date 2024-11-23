d6e62c566b0b02c4deab4a9a6a1f9542f82263d1
FilippoEnigma <97955822+FilippoEnigma@users.noreply.github.com>
2024-11-23 14:29:01 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='magisterka',
-    version='1.0.0',
+    version='1.0.7',
     description='Aplikacja Flask do zarządzania wydarzeniami i płatnościami.',
     author='Filip',
     author_email='your.email@example.com',
-    py_modules=['app'],  # Uwzględnij pojedyncze moduły jak app.py
-    include_package_data=True,  # Uwzględnia dane statyczne i pliki z MANIFEST.in
-    packages=find_packages(),
+    py_modules=['app'],
+    include_package_data=True,
     install_requires=[
         'flask',
         'configparser',
@@ -18,11 +17,11 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'magisterka=app:main',  # Odpowiada za uruchomienie funkcji main w app.py
+            'magisterka=app:main',
         ],
     },
+    packages=find_packages(),  # Użyj tego, jeśli masz strukturę katalogów jako paczki
     package_data={
-        "": ["*.ini"],  # Uwzględnij pliki konfiguracyjne
-        "templates": ["*.html"],  # Uwzględnij wszystkie pliki HTML z folderu templates
+        '': ['*.ini', 'templates/*.html'],  # Uwzględnij wszystkie pliki w templates
     },
 )
