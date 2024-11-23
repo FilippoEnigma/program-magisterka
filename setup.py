cd08809369616127b76ea7a7713ca0fb20306d6e
FilippoEnigma <97955822+FilippoEnigma@users.noreply.github.com>
2024-11-23 14:36:08 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='magisterka',
@@ -6,8 +6,8 @@ setup(
     description='Aplikacja Flask do zarządzania wydarzeniami i płatnościami.',
     author='Filip',
     author_email='your.email@example.com',
-    packages=find_packages(),  # Jeśli masz katalog, lepiej użyć find_packages()
-    include_package_data=True,  # Wymaga MANIFEST.in
+    py_modules=['app'],  # Jeśli masz tylko `app.py`
+    include_package_data=True,  # Wymaga MANIFEST.in do załączenia plików
     install_requires=[
         'flask',
         'configparser',
@@ -21,7 +21,7 @@ setup(
         ],
     },
     package_data={
-        '': ['*.ini'],  # Uwzględnij pliki .ini
-        'templates': ['*.html'],  # Uwzględnij pliki HTML w templates
+        '': ['*.ini'],  # Uwzględnij pliki konfiguracyjne
+        'templates': ['*.html'],  # Uwzględnij wszystkie pliki HTML w templates
     },
 )
