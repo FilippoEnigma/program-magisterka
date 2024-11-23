848453e70445ba0342d1898f6e7c42fef9deb8dc
FilippoEnigma <97955822+FilippoEnigma@users.noreply.github.com>
2024-11-23 13:21:29 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='magisterka',  
-    version='1.0.0',  
+    version='1.0.1',  
     description='Aplikacja Flask do zarządzania wydarzeniami i płatnościami.', 
     author='Filip',  
     author_email='your.email@example.com', 
